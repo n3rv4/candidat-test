7823a47f4ea6c53b0a278fd729f51e7dc137bcf2
someswararao <somesh.jagarapu@gmail.com>
2023-10-18 18:44:28 +0530

    Create a CRUD controller in easy admin and setting the field and Removed new comment button in comment list

---
+++ src/Controller/Admin/CommentsCrudController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Comments;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+
+class CommentsCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Comments::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        yield TextField::new('content');
+        yield DateTimeField::new('createdAt')->hideOnForm();
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return parent::configureActions($actions)
+            ->remove(Crud::PAGE_INDEX, Action::NEW);
+    }
+}
